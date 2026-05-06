bad3d6dd084e68935ad6529db7a4ea2509ae4e8e
Tripti Jha <triptijha2711@gmail.com>
2026-05-06 11:40:53 +0000

    Conversion of dataTypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "39"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+
+/*
+        "33" => 33
+        "88jajh" => NaN
+        true => 1; false =>0
+
+*/ 
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNum = 99
+let stringNum = String(someNum)
+console.log(stringNum);
+console.log(typeof stringNum);
+
